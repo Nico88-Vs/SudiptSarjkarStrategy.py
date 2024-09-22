f8fdc23305a02db5bda01b8bab796b6570824e20
Nico88-Vs <112125692+Nico88-Vs@users.noreply.github.com>
2024-09-22 18:12:46 +0200

    damn

--- SudiptStrategi.py
+++ SudiptStrategi.py
@@ -5,28 +5,34 @@ from urllib.parse import urlparse, parse_qs
 st.title("start")
 
 # Ottieni i parametri dalla query string dell'URL
-query_params = st.experimental_get_query_params()
-st.write(query_params)
-
-debug = st.query_params.keys()
-st.write(debug)
- 
-if "auth_code" in query_params:
-    auth_code = query_params["access_token"][0]  # Estrai il primo valore di auth_code
-    refreshcode = query_params["refresh_token"][0]
-    st.write(f"Authorization Code: {auth_code}")
+# query_params = st.experimental_get_query_params()
+# st.write(query_params)
+try:
+    debug = st.query_params.keys()
+    query_params = st.query_params.get("access_token")
     
-    if "AUTH" not in st.session_state:
-        st.session_state.AUTH = query_params["access_token"][0]
+    st.write( debug)
+     
+    if "auth_code" in query_params:
+        auth_code = query_params["access_token"][0]  # Estrai il primo valore di auth_code
+        refreshcode = query_params["refresh_token"][0]
+        st.write(f"Authorization Code: {auth_code}")
         
-    if "REFRESH" not in st.session_state:
-        st.session_state.REFRESH = query_params["refresh_token"][0]
+        if "AUTH" not in st.session_state:
+            st.session_state.AUTH = query_params["access_token"][0]
+            
+        if "REFRESH" not in st.session_state:
+            st.session_state.REFRESH = query_params["refresh_token"][0]
+        
+        # Qui puoi chiamare la funzione per ottenere il token
+        # ...logica per generare il token...
+    else:
+        st.write("Please authorize the app to continue.")
     
-    # Qui puoi chiamare la funzione per ottenere il token
-    # ...logica per generare il token...
-else:
-    st.write("Please authorize the app to continue.")
-    
+except :
+    pass
+
+
 connector = cnn.Connector()
 
 if st.button('connect'):
