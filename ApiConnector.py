357b80988cd8e5255be10fa5ca138ebf8b72d674
Nico88-Vs <112125692+Nico88-Vs@users.noreply.github.com>
2024-09-22 15:52:37 +0200

    try

--- ApiConnector.py
+++ ApiConnector.py
@@ -5,8 +5,8 @@ class Connector():
     
     def __init__(self):
         # Replace these values with your actual API credentials
-        self.client_id = "SPXXXXE7-100"
-        self.secret_key = "N********B"
+        self.client_id = "46OAC8DRLG-100"
+        self.secret_key = "94I7H2K4SZ"
         self.redirect_uri = "https://trade.fyers.in/api-login/redirect-uri/index.html"
         self.response_type = "code"  
         self.state = "sample_state"
--- SudiptStrategi.py
+++ SudiptStrategi.py
@@ -1,6 +1,34 @@
 import streamlit as st
 import ApiConnector as cnn
+from urllib.parse import urlparse, parse_qs
 
 st.title("start")
+
+# Ottieni i parametri dalla query string dell'URL
+query_params = st.experimental_get_query_params()
+st.write(query_params)
+
+debug = st.query_params.keys()
+st.write(debug)
+ 
+if "auth_code" in query_params:
+    auth_code = query_params["access_token"][0]  # Estrai il primo valore di auth_code
+    refreshcode = query_params["refresh_token"][0]
+    st.write(f"Authorization Code: {auth_code}")
+    
+    if "AUTH" not in st.session_state:
+        st.session_state.AUTH = query_params["access_token"][0]
+        
+    if "REFRESH" not in st.session_state:
+        st.session_state.REFRESH = query_params["refresh_token"][0]
+    
+    # Qui puoi chiamare la funzione per ottenere il token
+    # ...logica per generare il token...
+else:
+    st.write("Please authorize the app to continue.")
+    
+connector = cnn.Connector()
+
 if st.button('connect'):
-    cnn.Connector.connector.Connect()
+    
+    connector.connect()
